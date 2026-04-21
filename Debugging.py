03d5b37c3daf879793bcccf648356489cd81def1
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-04-21 21:32:41 +0100

    Add debugging techniques and tips
    Added debugging tips and techniques to Debugging.py.

---
+++ Debugging.py
@@ -0,0 +1,14 @@
+#Debugging can be done by going into your server and then adding/docs
+#a UI showing all current functions which is currently on the server with their command 
+#with this u can input different values for different parameters when needed and with each response a code is returned
+#u can look further into those codes by searching online 
+
+#you can also debug by adding breakpoints to stop code from running afetr the breakpoint 
+#this allows the pinpointing of bugs making it more efficient to find where the bugs are occuring 
+
+#you can also track the valuess of variables during runtime by:
+  #mkaing a breakpoint
+  #press F5 or Ctrl + shift + D
+  #a debug console should show up 
+  #while at a breakpoint hover over any variable to see its value 
+  
