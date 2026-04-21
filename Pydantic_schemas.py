a80c6601e72d90b99dded2e389ff2fe15ee3a25f
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-04-21 21:38:11 +0100

    Add comments for Pydantic schemas
    Add comments explaining Pydantic schemas and usage.

---
+++ Pydantic_schemas.py
@@ -0,0 +1,4 @@
+#Pydantic schemas are classes that define the structure and validaiton rules for data 
+#it gives structure by declaring variables and then restricting the data type that variable contain
+#a pydantic schema can only be made after importing BaseModel from pydantic and then inheriting from the BaseModel
+#then you make your class and then you make ur variables eg:
