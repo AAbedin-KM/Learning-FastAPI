f750a13ec94637730b361c1251683eb3b25f2da0
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-04-21 20:02:49 +0100

    Enhance installation guide with setup instructions
    Added detailed setup instructions for Python and FastAPI.

---
+++ Install and Setup Guide.py
@@ -0,0 +1,40 @@
+#Install and Setup guide
+
+#Prerequisites:
+  #Python 3.6 or higher (Python 3.8.2+ recommended)
+  #pip3 package manager
+
+#Check Python Version
+  #copy and paste into terminal:
+  python3 --version
+
+#Create virtual environment:
+  #copy and paste into terminal:
+  python3 -m venv fastapi_env #fastapi_venv is just the name of the virutal environment so it can be anything
+
+#To Activate Virtual Environment:
+  #if on MacOS/Linux:
+      #copy and paste into terminal:
+      source fastapi_env/bin/activate
+  #if on windows:
+      #copy and paste into terminal:
+      fastapi_env\Scripts\activate
+
+#To Install FastAPI:
+  #copy and paste into terminal:
+  pip3 install fastapi
+
+#To Install Uvicorn (ASGI Server)
+  #copy and paste into terminal:
+  pip3 install uvicorn[standard]
+
+#To Start Development Server:
+  #copy and paste into terminal:
+  uvicorn main:app --reload
+
+#To see your API running click on the localhost link which will be outputted on the terminal whenever u start the development server
+#to be able to interact with your code visually:
+  #go on ur server and then type /docs
+
+
+
