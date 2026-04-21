3fcfdc5acd980cbb24d49051509e7115f40b85dc
Aliff Abedin <aliffahnaff2020@gmail.com>
2026-04-21 20:34:57 +0100

    Add FastAPI user data retrieval and welcome endpoint
    Implemented a FastAPI application with dynamic user data retrieval based on user ID and a welcome endpoint.

---
+++ path_parameters.py
@@ -0,0 +1,28 @@
+#import FastAPI
+from fastapi import FastAPI
+
+#initilaised FastAPI application
+app = FastAPI()
+
+@app.get("/userdata/{id}") #decorator is used to create an endpoint at the path that listens to GET commands
+#{id}
+def data_on_user(id:int): #restricts id to only be a integer value
+    if id == 1: #if id is 1 then when going onto the path then this is returned
+        return {"data":
+        {"firstname" : "John",
+         "lastname" : "Smith",
+         "id" : id}}
+    elif id == 2: #if id is 2 then when going onto the path then this is returned
+        return {"data":
+        {"firstname" : "Klein",
+         "lastname" : "Moretti",
+         "id" : id}}
+
+#if the following path is written then the following function is not returned 
+#this is because aftr the dynamic route , FastAPI will attempt to match welcome to the integer data type 
+#this happens as everything a path is opened up , FastAPI reads th code line by line and so id being set to being only an integer is done before userdata/welcome is made
+#this would fail leading to different output than from what is wanted
+@app.get("/userdata/welcome")
+def welcome_user():
+    return "welcome user"
+
